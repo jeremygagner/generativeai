d987910747dea5339406551e2c1774a66d2dd0e2
Jérémy Gagner <1531666@colval.qc.ca>
2023-03-28 14:31:50 -0400

    setup gh-pages

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,7 +13,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/ai", {
+        const response = await fetch("https://generous-ai-generator-backend.onrender.com/api/v1/ai", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ prompt: form.prompt }),
@@ -34,7 +34,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://generous-ai-generator-backend.onrender.com/api/v1/post", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(form),
